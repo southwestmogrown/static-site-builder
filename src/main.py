91a1501ef271c7e0e46159e5e815eb47f65759cf
southwestmogrown <southwestmogrown@gmail.com>
2026-02-08 18:56:26 -0600

    added generation of public directory from static directory

--- src/main.py
+++ src/main.py
@@ -1,8 +1,37 @@
+import os, shutil
 from textnode import *
 
+def clean_and_populate_public(src, dest):
+  if os.path.exists(dest):
+    shutil.rmtree(dest)
+
+
+  os.mkdir(dest)
+  
+  src_paths = os.listdir(src)
+  for path in src_paths:
+    new_src = os.path.join(src, path)
+    if os.path.isfile(new_src):
+      shutil.copy(new_src, dest)
+    else:
+      new_dest = os.path.join(dest, path)
+      clean_and_populate_public(new_src, new_dest)
+
+
+
+
+
+    
+
 def main():
-  pass
+  clean_and_populate_public("static", "public")
 
 
 
 
+
+
+
+
+if __name__ == "__main__":
+   main()
